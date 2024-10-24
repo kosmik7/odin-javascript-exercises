f701487a7fad6a1df908d45ab347e34ca3f23bea
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-24 15:37:23 +0200

    Complete the exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (!Number.isInteger(number1) || !Number.isInteger(number2)) return 'ERROR';
+    if (number1 < 0 || number2 < 0) return 'ERROR';
 
+    const array = [number1, number2].sort((a, b) => a - b)
+    sortedNumber1 = array[0]
+    sortedNumber2 = array[1]
+    let sum = 0
+    
+    for (sortedNumber1; sortedNumber1 <= sortedNumber2; sortedNumber1++) {
+        sum += sortedNumber1
+    }
+    return sum
 };
 
 // Do not edit below this line
