c5baecf93ad9bd531cdfb24875614c820d15ed49
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-24 12:32:51 +0200

    Complete the exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const set = new Set(args)
+    return result = array.filter((item) => !set.has(item));
 };
 
 // Do not edit below this line
