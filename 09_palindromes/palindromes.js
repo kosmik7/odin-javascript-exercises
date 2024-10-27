b0466ec4c298b45773ed8cad7f25e49cdaf2fd86
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-27 18:26:02 +0100

    Complete the exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, '')
+    const reversedString = cleanString.split('').reverse().join('')
+    return reversedString === cleanString
 };
 
 // Do not edit below this line
