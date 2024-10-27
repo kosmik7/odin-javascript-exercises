7cbd0a41dfa29eea6fbc00cb78d5b9d2cefeecce
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-27 18:47:11 +0100

    Complete the exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (typeof number !== 'number') parseInt(number)
+    if (number < 0) return 'OOPS'
 
+    const fibonacciSequence = [0, 1]
+    for (let i = 1; i <= number; i++) {
+        fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i])
+    }
+    return fibonacciSequence[number]
 };
 
+console.log(fibonacci(20))
 // Do not edit below this line
 module.exports = fibonacci;
