55576fe760229d0b4339a351ea3c7e52224ac092
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-27 19:43:53 +0100

    Complete the exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
+    
+    return array.reduce((oldest, person) => {
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        console.log(oldest)
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
