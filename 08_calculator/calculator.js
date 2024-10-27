639fe0e5dd1509751b938d1c3d161ed0d293cae7
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-27 14:47:13 +0100

    Complete the exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, number) => sum += number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, number) => sum *= number);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+  for (let i = 1; i <= a; i++) {
+    sum *= i;
+  }
+  return sum
 };
 
 // Do not edit below this line
